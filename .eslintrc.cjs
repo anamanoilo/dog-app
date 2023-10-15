b3178de0cbfaf5fc51e2ca3756c7da3dc7f6abe9
Anastasia <anastasia.nortsova@gmail.com>
2023-10-15 22:55:17 +0200

    feat: add material ui

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,16 +5,27 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
     'prettier'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    }
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json',
+    sourceType: 'module'
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true }
-    ],
+    'import/prefer-default-export': 0,
+    'react/react-in-jsx-scope': 0,
     'prettier/prettier': [
       'warn',
       {
