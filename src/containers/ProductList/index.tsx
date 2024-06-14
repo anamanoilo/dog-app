35a1c9c46d40da853f0fb1d85bd86c03be2f29d2
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2024-06-15 02:01:44 +0300

    delete redundant files

--- src/containers/ProductList/index.tsx
+++
@@ -1,7 +0,0 @@
-import { useGetProductsQuery } from '../../hooks/useGetProductsQuery';
-
-export const Productlist = () => {
-  const { data } = useGetProductsQuery();
-  console.log('data:', data);
-  return <div>Product List</div>;
-};
